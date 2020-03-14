faa88d1dc3608e061aff71148f781c27f720d935
Akinnifesi Oluwatosin <daleks1987@gmail.com>
2020-03-14 12:25:52 -0500

    Default Changelist

--- Views/assets/ministries/js/ministries-background.js
+++ Views/assets/ministries/js/ministries-background.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    let gallerySlideCounter = 0;
-    let galleryDiv ='';
-    let modalDiv ='';
     var photoAdd = '/photoalbums/find';
 
     function getPhoto() {
@@ -20,12 +17,9 @@ $(document).ready(function(){
     function AjaxSucceeded(result) {
         let imgNames = [];
         $.each(result, function (k, dbData) {
-            //for gallery image
-            // galleryDiv += '<div class="mbr-gallery-item mbr-gallery-item--p2" data-video-url="false" data-tags="' + dbData.albums[0].u_name + '">';
-            // galleryDiv += '<img src="assets/images/gallery/' + dbData+ '" alt="mfm_gallery" title="' + dbData.title + '"/>';
-            // if(dbData.albums[0].u_name.toLowerCase() === "ministry")
             imgNames.push(dbData);
         });
+        debugger;
         $(".grid li").each((i, li)=>{
             let bg = 'assets/images/gallery/' + imgNames[Math.floor(Math.random() * imgNames.length) + 1  ].imgName ;
             $(li).css('background-image', 'url('  + bg + ')');
