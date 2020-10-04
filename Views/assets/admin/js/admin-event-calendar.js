98379902bc3c09fe93deca224e80d38bb2983f44
Akinnifesi Oluwatosin <daleks1987@gmail.com>
2020-10-04 14:05:57 -0500

    removed error causing close button

--- Views/assets/admin/js/admin-event-calendar.js
+++ Views/assets/admin/js/admin-event-calendar.js
@@ -93,8 +93,8 @@ function loadData() {
                 html +='<!-- large modal -->\n';
 
                 html2 += '<img src="' + content.event_imgPath + '" alt="mfmsouth-church-events" style="width:100%">\n';
-                html2 += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>\n';
-                html2 += '</button>';
+//                 html2 += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>\n';
+//                 html2 += '</button>';
 
                 getModal = document.createElement('div');
                 getModal.id = "largeModal"+getModalHeaderCounter;
